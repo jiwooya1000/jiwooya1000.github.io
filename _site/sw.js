a2513d227e42d1e91527ab774933f637bd052c36
Jiwoo Hong <jiwooya1000@gmail.com>
2024-03-06 00:09:09 +0900

    Main page minor edit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240306.000420';
+const cacheName = 'chirpy-20240306.000842';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
