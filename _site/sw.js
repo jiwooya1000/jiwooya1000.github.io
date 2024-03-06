d4f3c44ffc58abc7c536ec424cdb6cef79c3cda3
Jiwoo Hong <jiwooya1000@gmail.com>
2024-03-06 16:12:23 +0900

    Update AlpacaEval result figure

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240306.000842';
+const cacheName = 'chirpy-20240306.160620';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
