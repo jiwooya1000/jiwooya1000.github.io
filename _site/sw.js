62a43f1d94c25a0aee6c4544a92033d27a5103df
Jiwoo Hong <jiwooya1000@gmail.com>
2024-03-07 16:22:00 +0900

    Hyperlink color edit

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240307.161947';
+const cacheName = 'chirpy-20240307.162139';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
