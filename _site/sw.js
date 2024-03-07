36c773f2247ceffb37e9564ace534439acde23d7
Jiwoo Hong <jiwooya1000@gmail.com>
2024-03-07 16:20:08 +0900

    Add pdf link to ORPO paper

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240306.160620';
+const cacheName = 'chirpy-20240307.161947';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
