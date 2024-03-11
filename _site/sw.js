c773ee92f333fcc443179dee744e854f11a32b0b
Jiwoo Hong <jiwooya1000@gmail.com>
2024-03-11 17:31:03 +0900

    Edit equation and main fig

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20240307.162139';
+const cacheName = 'chirpy-20240311.173027';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
